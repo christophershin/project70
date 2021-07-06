a0a2b12c85dfeb7d11e755525a8041f9262fb808
christophershin <72586810+christophershin@users.noreply.github.com>
2021-07-05 19:16:02 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'React'
+import{StyleSheet, text, view, Image} from 'react-native';
+import { createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+
